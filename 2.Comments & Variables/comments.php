b6ac024889d33048f471cefc1c5d0ce8b731b741
adarshsharma1350 <111039432+adarshsharma1350@users.noreply.github.com>
2024-02-09 03:40:41 +0530

    Create comments.php

---
+++ 2.Comments & Variables/comments.php
@@ -0,0 +1,20 @@
+<!-- It's comments -->
+
+<?php
+
+// It's a single line comment example
+
+# It's also use for comment in the php 
+
+/*It's 
+a 
+multiline 
+comment
+example.*/
+
+// Comments in the Middle of the code
+$x = 5 /* + 15 */ + 5 + 65;
+echo $x;
+?>
+
+ 

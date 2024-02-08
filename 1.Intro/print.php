febb234c87568e571236d7f6394ddb05866274db
adarshsharma1350 <111039432+adarshsharma1350@users.noreply.github.com>
2024-02-09 03:33:34 +0530

    Create print.php

---
+++ 1.Intro/print.php
@@ -0,0 +1,15 @@
+<!-- Printing Statement -->
+
+<?php
+
+// using print keyword
+print("hello world");
+
+// using echo keyword
+echo "PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.";
+
+// check php version
+echo phpversion();
+
+
+?>
